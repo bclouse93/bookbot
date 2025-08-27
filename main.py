9ac08978f9be88444426f396fe945e842b5c0781
NephilimofLight <heinb93@gmail.com>
2025-08-26 18:09:58 -0700

    B: create py book progrm

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(books):
+
+    with open(books) as f:
+    # do something with f (the file) here
+
+    # f is a file object
+     file_contents = f.read()
+     return file_contents
+    
+def main(get_book_text):
+   book_path = "books/frankenstein.txt"
+   book_text = get_book_text(book_path)
+   print(book_text)
+main(get_book_text)
