dd03fc43bb5507b0a05c49816d41cfa306ba1f58
NephilimofLight <heinb93@gmail.com>
2025-08-27 13:52:05 -0700

    B2: char count

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from stats import word_count
-
+from stats import get_char_count
 def get_book_text(books):
 
     with open(books) as f:
-    # 
-
-    # f is a file object
      file_contents = f.read()
      return file_contents
     
-
-
 def main():
    book_path = "books/frankenstein.txt"
    book_text = get_book_text(book_path)
    num_words = word_count(book_text)
+   num_chars = get_char_count(book_text)
    print(f"{num_words} words found in the document")
+   print(num_chars)
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,17 @@
-from stats import num_words
-    
 def word_count(book_text):
     words = book_text.split()
     num_words = len(words)
     return num_words
      #print (f"{word_count} words found in the document")   
 
-
+def get_char_count(book_text):
+    char_count = book_text.lower()
+    char_dict={}
+    for char in char_count: 
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+print(get_char_count("hello"))
+     #print (f"{word_count} words found in the document")   
